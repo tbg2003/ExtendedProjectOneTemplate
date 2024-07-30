c1c6bb903f4e33f24890d016c694ec182f57e0b2
Tom Burt-Gray <tom.burt.gray@Toms-Laptop.local>
2024-07-30 11:01:20 +0100

    passing tests for 200 OK response

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,22 +1,32 @@
 package controllers
 
-import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents, Request}
 
 import javax.inject.{Inject, Singleton}
+import scala.concurrent.Future
 
 
 @Singleton
 class ApplicationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController{
-  def index(): Action[AnyContent] = TODO
 
-  def create(): Action[AnyContent] = TODO
+  def index(): Action[AnyContent] = Action.async{implicit request: Request[AnyContent] =>
+    Future.successful(Ok)
+  }
 
+  def create(): Action[AnyContent] = Action.async{implicit request: Request[AnyContent] =>
+    Future.successful(Ok)
+  }
 
-  def read(id: String): Action[AnyContent] = TODO
+  def read(id: String): Action[AnyContent] = Action.async{implicit request: Request[AnyContent] =>
+    Future.successful(Ok)
+  }
 
+  def update(id: String): Action[AnyContent] = Action.async{implicit request: Request[AnyContent] =>
+    Future.successful(Ok)
+  }
 
-  def update(id: String): Action[AnyContent] = TODO
+  def delete(id: String): Action[AnyContent] = Action.async{implicit request: Request[AnyContent] =>
+    Future.successful(Ok)
+  }
 
-
-  def delete(id: String): Action[AnyContent] = TODO
 }
--- test/controllers/ApplicationControllerSpec.scala
+++ test/controllers/ApplicationControllerSpec.scala
@@ -1,6 +1,10 @@
 package controllers
 
 import baseSpec.BaseSpecWithApplication
+import play.api.test.FakeRequest
+import play.api.http.Status
+import play.api.test.Helpers._
+
 
 class ApplicationControllerSpec extends BaseSpecWithApplication{
   val TestApplicationController = new ApplicationController(
@@ -8,21 +12,46 @@ class ApplicationControllerSpec extends BaseSpecWithApplication{
   )
 
   "ApplicationController .index()" should {
-    ???
+    val result = TestApplicationController.index()(FakeRequest())
+
+    "return 200 OK response" in {
+      status(result) shouldBe Status.OK
+    }
   }
 
   "ApplicationController .create()" should {
-    ???
+    val result = TestApplicationController.create()(FakeRequest())
+
+    "return 200 OK response" in {
+      status(result) shouldBe Status.OK
+    }
   }
 
   "ApplicationController .read()" should {
-    ???
+    val fakeId:String = "1234"
+    val result = TestApplicationController.read(fakeId)(FakeRequest())
+
+    "return 200 OK response" in {
+      status(result) shouldBe Status.OK
+    }
   }
+
   "ApplicationController .update()" should {
-    ???
+    val fakeId:String = "1234"
+    val result = TestApplicationController.update(fakeId)(FakeRequest())
+
+    "return 200 OK response" in {
+      status(result) shouldBe Status.OK
+    }
   }
+
   "ApplicationController .delete()" should {
-    ???
+    val fakeId:String = "1234"
+    val result = TestApplicationController.delete(fakeId)(FakeRequest())
+
+    "return 200 OK response" in {
+      status(result) shouldBe Status.OK
+    }
   }
 
 }
