55ee0c84474ceb04c281d5e26c4d483a8b08119f
Tom Burt-Gray <tom.burt.gray@Toms-Laptop.local>
2024-07-30 10:43:45 +0100

    new outline for routes and tests

---
+++ app/controllers/ApplicationController.scala
@@ -0,0 +1,22 @@
+package controllers
+
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+
+import javax.inject.{Inject, Singleton}
+
+
+@Singleton
+class ApplicationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController{
+  def index(): Action[AnyContent] = TODO
+
+  def create(): Action[AnyContent] = TODO
+
+
+  def read(id: String): Action[AnyContent] = TODO
+
+
+  def update(id: String): Action[AnyContent] = TODO
+
+
+  def delete(id: String): Action[AnyContent] = TODO
+}
