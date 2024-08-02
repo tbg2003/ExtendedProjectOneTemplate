dc254c3c110be464b7a3ca15eecd7d9ccf543f6f
Tom Burt-Gray <tom.burt.gray@Toms-Laptop.local>
2024-08-02 10:16:47 +0100

    new style sheet and view .html

--- app/models/DataModel.scala
+++ app/models/DataModel.scala
@@ -1,4 +1,6 @@
 package models
+import play.api.data._
+import play.api.data.Forms._
 import play.api.libs.json.{Json, OFormat}
 
 case class DataModel(_id: String,
@@ -8,6 +10,15 @@ case class DataModel(_id: String,
 
 object DataModel {
   implicit val formats: OFormat[DataModel] = Json.format[DataModel]
+
+  val dataModelForm: Form[DataModel] = Form {
+    mapping(
+      "_id" -> text,
+      "title" -> text,
+      "subtitle" -> text,
+      "pageCount" -> number,
+    )(DataModel.apply)(DataModel.unapply)
+  }
 }
 
 
