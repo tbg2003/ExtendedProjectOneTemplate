ca65239979e028bd860eae5d027e8ec39a72ff71
Tom Burt-Gray <tom.burt.gray@Toms-Laptop.local>
2024-08-07 13:28:38 +0100

    cusotm login constraints

--- app/models/Login.scala
+++ app/models/Login.scala
@@ -2,6 +2,7 @@ package models
 
 import play.api.data.Form
 import play.api.data.Forms.{email, mapping, nonEmptyText}
+import play.api.data.validation.{Constraint, Invalid, Valid, ValidationError}
 import play.api.libs.json.{Json, OFormat}
 
 case class Login(
@@ -11,10 +12,73 @@ case class Login(
 object Login{
   implicit val formats: OFormat[Login] = Json.format[Login]
 
+  val passwordMinLength: Constraint[String] = Constraint("constraints.password.minLength")({
+    plainText =>
+      if (plainText.length >= 8) {
+        Valid
+      } else {
+        Invalid(ValidationError("must be at least 8 characters long"))
+      }
+  })
+
+  val passwordHasUpperCase: Constraint[String] = Constraint("constraints.password.upperCase")({
+    plainText =>
+      if (plainText.exists(_.isUpper)) {
+        Valid
+      } else {
+        Invalid(ValidationError("must contain at least one uppercase letter"))
+      }
+  })
+
+  val passwordHasLowerCase: Constraint[String] = Constraint("constraints.password.lowerCase")({
+    plainText =>
+      if (plainText.exists(_.isLower)) {
+        Valid
+      } else {
+        Invalid(ValidationError("must contain at least one lowercase letter"))
+      }
+  })
+
+  val passwordHasDigit: Constraint[String] = Constraint("constraints.password.digit")({
+    plainText =>
+      if (plainText.exists(_.isDigit)) {
+        Valid
+      } else {
+        Invalid(ValidationError("must contain at least one digit"))
+      }
+  })
+
+  val passwordHasSpecialChar: Constraint[String] = Constraint("constraints.password.specialChar")({
+    plainText =>
+      val specialChars = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/`~"
+      if (plainText.exists(ch => specialChars.contains(ch))) {
+        Valid
+      } else {
+        Invalid(ValidationError("must contain at least one special character"))
+      }
+  })
+
+  val passwordConstraints: Constraint[String] = Constraint("constraints.password")({
+    plainText =>
+      val constraints = Seq(
+        passwordMinLength,
+        passwordHasUpperCase,
+        passwordHasLowerCase,
+        passwordHasDigit,
+        passwordHasSpecialChar
+      )
+
+      val errors = constraints.flatMap(constraint => constraint(plainText) match {
+        case Invalid(errors) => errors
+        case _ => Nil
+      })
+      if (errors.isEmpty) Valid
+      else Invalid(errors)
+  })
   val loginForm: Form[Login] = Form {
     mapping(
       "userName" -> email,
-      "password" -> nonEmptyText,
+      "password" -> nonEmptyText.verifying(passwordConstraints)
     )(Login.apply)(Login.unapply)
   }
 }
